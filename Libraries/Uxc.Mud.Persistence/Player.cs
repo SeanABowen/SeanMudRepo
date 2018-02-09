5d5f9d9b2e662d85f3aaea45ef587084f355378b
Nick Shepherd <nshepherd@uxceclipse.com>
2018-02-09 10:42:17 -0800

    Added DOB to player.

--- Libraries/Uxc.Mud.Persistence/Player.cs
+++ Libraries/Uxc.Mud.Persistence/Player.cs
@@ -1,9 +1,12 @@
-﻿namespace Uxc.Mud.Persistence
+﻿using System;
+
+namespace Uxc.Mud.Persistence
 {
     public class Player
     {
         public string Name { get; set; }
         public int Health { get; set; }
         public Area CurrentArea { get; set; }
+        public DateTime Dob { get; set; }
     }
 }
